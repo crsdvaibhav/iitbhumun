17165e86c3550d118e9b33ae0c9159dca75e68ea
Naman Agrawal <naman.agrawal.na05@gmail.com>
2022-11-22 18:35:03 +0530

    thankyou page error

--- pages/thankyou.js
+++ pages/thankyou.js
@@ -1,15 +1,14 @@
-import Footer from "../components/Footer";
-import ActiveNavbar from "../components/ActiveNavbar";
-
+import { Navbar } from '@material-tailwind/react';
+import Footer from '../components/Footer';
 
 export default function Thankyou() {
-    return (
-        <div className="bg-[#F5F5F5]">
-            <ActiveNavbar />
-            <div className="flex justify-center items-center h-[100vh] w-full text-6xl">
-                Thankyou!
-            </div>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="bg-[#F5F5F5]">
+      <Navbar />
+      <div className="flex justify-center items-center h-[100vh] w-full text-6xl">
+        Thankyou!
+      </div>
+      <Footer />
+    </div>
+  );
 }
