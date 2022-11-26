9fe66fee7eeb74297d915d9e44756f20c75709e5
Naman Agrawal <naman.agrawal.na05@gmail.com>
2022-11-26 19:19:35 +0530

    seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
 import { ThemeProvider } from '@material-tailwind/react';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
+      <Head>
+        <title>IITBHU MUN</title>
+        <meta
+          name="keywords"
+          content="modelunitednations ,mun ,unitednations ,imun ,youth ,internationalconference ,internationalmun ,un ,diplomacy ,munconference ,conference ,onlinemun ,international ,onlineconference ,leaders ,globalopportunity ,opportunity ,youngleaders ,online ,webinar ,modelun ,debate ,internationalrelations ,youthleader ,youthpower ,diversity ,asiamun ,india ,malaysia ,identitycrisis ,iitbhu ,iitdelhi ,iitbombay ,iit ,iitkanpur ,iitkgp ,iitmadras ,iitroorkee ,iitmemes ,iitguwahati ,iithyderabad ,bhu ,iitkharagpur ,iitian ,iitindore ,iitpatna ,iitropar ,iitmandi ,iitjodhpur ,memes ,iitdhanbad ,iitjammu ,engineeringmemes ,iitgoa ,collegememes ,iitb ,memesdaily ,gpat ,meme ,sarcasm"
+        />
+        <meta
+          name="description"
+          content="Congratulations! We’re very excited that you will be doing Model United Nations for the first time. You are about to embark on a life-changing journey that will help you develop confidence in leading others, a stronger awareness of global issues, and the chance to make new friends from around the world. Model UN is a […"
+        />
+        <meta property="og:url" content="https://iitbhumun.com/" />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="/images/Vector-dark.png" />
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
