ee50e077aec8e746c50205f48c88038ffe93ba64
Naman Agrawal <naman.agrawal.na05@gmail.com>
2022-11-26 19:13:50 +0530

    kw

--- pages/_document.js
+++ pages/_document.js
@@ -7,13 +7,12 @@ export default function Document() {
       <Head>
         <title>IITBHU MUN</title>
         <meta
+          name="keywords"
+          content="modelunitednations ,mun ,unitednations ,imun ,youth ,internationalconference ,internationalmun ,un ,diplomacy ,munconference ,conference ,onlinemun ,international ,onlineconference ,leaders ,globalopportunity ,opportunity ,youngleaders ,online ,webinar ,modelun ,debate ,internationalrelations ,youthleader ,youthpower ,diversity ,asiamun ,india ,malaysia ,identitycrisis ,iitbhu ,iitdelhi ,iitbombay ,iit ,iitkanpur ,iitkgp ,iitmadras ,iitroorkee ,iitmemes ,iitguwahati ,iithyderabad ,bhu ,iitkharagpur ,iitian ,iitindore ,iitpatna ,iitropar ,iitmandi ,iitjodhpur ,memes ,iitdhanbad ,iitjammu ,engineeringmemes ,iitgoa ,collegememes ,iitb ,memesdaily ,gpat ,meme ,sarcasm"
+        />
+        <meta
           name="description"
           content="Congratulations! We’re very excited that you will be doing Model United Nations for the first time. You are about to embark on a life-changing journey that will help you develop confidence in leading others, a stronger awareness of global issues, and the chance to make new friends from around the world. Model UN is a […"
-        />
-        <meta property="og:title" content="IITBHU MUN - HOME" />
-        <meta
-          property="og:description"
-          content="Add a shopping cart to your site in minutes. Works with any site builder, CMS, and framework. 20 000+ merchants trust our e-commerce solution for their website. Join them!"
         />
         <meta property="og:url" content="https://iitbhumun.com/" />
         <meta property="og:type" content="website" />
