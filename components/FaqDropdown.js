506bb3f82a3795c2a024b096a3e5cdf471cf753a
crsdvaibhav <vaibhav.krpandey.che21@itbhu.ac.in>
2022-11-28 00:21:19 +0530

    FAQs done

--- components/FaqDropdown.js
+++ components/FaqDropdown.js
@@ -1,18 +1,27 @@
-import { useState } from "react"
+import { Fragment, useState } from "react";
+import {
+    Accordion,
+    AccordionHeader,
+    AccordionBody,
+} from "@material-tailwind/react";
 
-export default function Dropdown(props){
+export default function Dropdown(props) {
+    const [open, setOpen] = useState(0);
 
-    const [isActive, setIsActive] = useState(false)
+    const handleOpen = (value) => {
+        setOpen(open === value ? 0 : value);
+    };
 
-    return(
-        <div className={isActive ? "flex flex-col mx-[8.188rem] mb-[2.938rem] w-[34.313rem] h-[12.563rem] bg-white shadow-black/50 shadow-md rounded-lg px-4 py-2 font-custom text-[1.375rem]" : "flex flex-col mx-[8.188rem] mb-[2.938rem] w-[34.313rem] h-[4rem] bg-white shadow-black/50 shadow-md rounded-lg px-4 py-2 font-custom text-[1.375rem]"}>
-            <div className="flex justify-between items-center overflow-hidden">
-                <div className={isActive ? "py-2 font-semibold" : "py-2"}>Q.{props.id} {props.question}</div>
-                <div className="py-2 text-[20px] font-bold"><button onClick={()=>setIsActive(!isActive)}>{isActive ? "-" : "+"}</button></div>
-            </div>
-            {isActive ? <div className="h-[100%] pb-2 font-custom text-[1.125rem] font-light flex items-center justify-center border-t-[1px] border-black/30"> 
-                {props.ans}
-            </div> : <></>}
-        </div>
-    )
+    return (
+        <Fragment>
+            <Accordion open={open === 1} className={open ? "flex flex-col mx-[8.188rem] mb-[2.938rem] w-[34.313rem] h-[12.563rem] bg-white shadow-black/10 shadow-xl rounded-lg px-4 py-2" : "flex flex-col mx-[8.188rem] mb-[2.938rem] w-[34.313rem] h-[4rem] bg-white shadow-black/10 shadow-xl rounded-lg px-4 py-2}?"}>
+                <AccordionHeader onClick={() => handleOpen(1)} className="text-[1.375rem] font-custom">
+                    Q.{props.id} {props.question}
+                </AccordionHeader>
+                <AccordionBody className="text-[1.125rem] font-custom">
+                    {props.ans}
+                </AccordionBody>
+            </Accordion>
+        </Fragment>
+    );
 }
