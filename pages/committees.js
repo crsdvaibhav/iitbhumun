27aade02f8894a97b7b3b3c45af8bb224edc2356
crsdvaibhav <vaibhav.krpandey.che21@itbhu.ac.in>
2022-11-30 18:13:00 +0530

    Committee page with modal done

--- pages/committees.js
+++ pages/committees.js
@@ -1,82 +1,23 @@
-import NavBar from '../components/Navbar.js';
-import Footer from '../components/Footer.js';
-import Image from 'next/image.js';
-import {
-  Popover,
-  PopoverHandler,
-  PopoverContent,
-} from '@material-tailwind/react';
-import { Fragment, useState } from 'react';
-import DialogBox from '../components/Dialog';
+import NavBar from "../components/Navbar.js";
+import Footer from "../components/Footer.js";
+import Image from "next/image.js";
+import CommitteeCircle from "../components/CommitteeCircle.js";
 
 export default function Committees() {
-  const [open, setOpen] = useState(false);
-
-  const handleChange = () => setOpen(!open);
-
-  return (
-    <div className="bg-[#F5F5F5]">
-      <NavBar navbar={true} />
-      <Fragment>
-        <div className="h-[100vh] flex justify-center">
-          <div className="w-1/3 h-[100vh] flex flex-col items-end justify-center">
-            <div className="flex pb-4">
-              <div
-                onClick={handleChange}
-                className="mr-4 w-[11.313rem] h-[11.313rem] cursor-pointer rounded-full shadow-[#189BA5]/20 shadow-xl top-[12.5rem] left-[9.836rem] bg-white"
-              >
-                <div className="flex flex-col items-center">
-                  <div className="pt-[1.688rem]">
-                    <Image src="/images/ecosoc.svg" width={114} height={95} />
-                  </div>
-                  <div className="font-bold font-sans text-[1.2rem] text-[#189BA5]">
-                    ECOSOC
-                  </div>
+    return (
+        <div className="bg-[#F5F5F5]">
+            <NavBar navbar={true} />
+            <div className="h-[100vh] flex justify-center">
+                <div className="w-1/3 h-[100vh] flex flex-col items-end justify-center">
+                    <div className="flex pb-4">
+                        <CommitteeCircle key={1} src={"/images/ecosoc.svg"} title={"ECOSOC"}/>
+                        <CommitteeCircle key={2} src={"/images/unodc.svg"} title={"UNODC"}/>
+                    </div>
+                    <div className="flex pt-4">
+                        <CommitteeCircle key={3} src={"/images/aippm.svg"} title={"AIPPM"}/>
+                        <CommitteeCircle key={4} src={"/images/cc.svg"} title={"Crisis"}/>
+                    </div>
                 </div>
-              </div>
-              <div
-                onClick={handleChange}
-                className="ml-4 w-[11.313rem] h-[11.313rem] cursor-pointer cursor-pointer  rounded-full shadow-[#189BA5]/20 shadow-xl top-[25.5rem] left-[17.836rem] bg-white"
-              >
-                <div className="flex flex-col items-center">
-                  <div className="pt-[1.688rem]">
-                    <Image src="/images/unodc.svg" width={114} height={95} />
-                  </div>
-                  <div className="font-bold font-sans text-[1.2rem] text-[#189BA5]">
-                    UNODC
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="flex pt-4">
-              <div
-                onClick={handleChange}
-                className="mr-4 w-[11.313rem] h-[11.313rem] cursor-pointer rounded-full shadow-[#189BA5]/20 shadow-xl top-[35.875rem] left-[29.398rem] bg-white"
-              >
-                <div className="flex flex-col items-center">
-                  <div className="pt-[1.688rem]">
-                    <Image src="/images/aippm.svg" width={114} height={95} />
-                  </div>
-                  <div className="font-bold font-sans text-[1.2rem] text-[#189BA5]">
-                    AIPPM
-                  </div>
-                </div>
-              </div>
-              <div
-                onClick={handleChange}
-                className="ml-4 w-[11.313rem] h-[11.313rem] cursor-pointer rounded-full shadow-[#189BA5]/20 shadow-xl top-[42.563rem] left-[45.336rem] bg-white"
-              >
-                <div className="flex flex-col items-center">
-                  <div className="pt-[1.688rem]">
-                    <Image src="/images/cc.svg" width={126} height={90} />
-                  </div>
-                  <div className="font-bold font-sans text-[0.8rem] text-[#189BA5] text-center">
-                    Crisis<br></br>Committee
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
 
           <div className="w-1/3 h-[100vh] flex items-center justify-center">
             <div className="text-center">
@@ -87,68 +28,18 @@ export default function Committees() {
             </div>
           </div>
 
-          <div className="w-1/3 h-[100vh] flex flex-col items-start justify-center">
-            <div className="flex pb-4">
-              <div
-                onClick={handleChange}
-                className="mr-4 w-[11.313rem] h-[11.313rem] cursor-pointer rounded-full shadow-[#189BA5]/20 shadow-xl top-[42.563rem] left-[63.461rem] bg-white"
-              >
-                <div className="flex flex-col items-center">
-                  <div className="pt-[1.688rem]">
-                    <Image src="/images/disec.svg" width={114} height={95} />
-                  </div>
-                  <div className="font-bold font-sans text-[1.2rem] text-[#189BA5]">
-                    DISEC
-                  </div>
+                <div className="w-1/3 h-[100vh] flex flex-col items-start justify-center">
+                    <div className="flex pb-4">
+                        <CommitteeCircle key={5} src={"/images/disec.svg"} title={"DISEC"}/>
+                        <CommitteeCircle key={6} src={"/images/uncsw.svg"} title={"UNCSW"}/>
+                    </div>
+                    <div className="flex pt-4">
+                        <CommitteeCircle key={7} src={"/images/wto.svg"} title={"WTO"}/>
+                        <CommitteeCircle key={8} src={"/images/ls.svg"} title={"Lok Sabha"}/>
+                    </div>
                 </div>
-                <DialogBox open={open} handler={handleChange} />
-              </div>
-              <div
-                onClick={handleChange}
-                className="ml-4 w-[11.313rem] h-[11.313rem] cursor-pointer rounded-full shadow-[#189BA5]/20 shadow-xl top-[42.563rem] left-[63.461rem] bg-white"
-              >
-                <div className="flex flex-col items-center">
-                  <div className="pt-[1.688rem]">
-                    <Image src="/images/uncsw.svg" width={114} height={95} />
-                  </div>
-                  <div className="font-bold font-sans text-[1.2rem] text-[#189BA5]">
-                    UNCSW
-                  </div>
-                </div>
-              </div>
             </div>
-            <div className="flex pt-4">
-              <div
-                onClick={handleChange}
-                className="mr-4 w-[11.313rem] h-[11.313rem] cursor-pointer rounded-full shadow-[#189BA5]/20 shadow-xl top-[42.563rem] left-[63.461rem] bg-white"
-              >
-                <div className="flex flex-col items-center">
-                  <div className="pt-[1.688rem]">
-                    <Image src="/images/wto.svg" width={114} height={95} />
-                  </div>
-                  <div className="font-bold font-sans text-[1.2rem] text-[#189BA5]">
-                    WTO
-                  </div>
-                </div>
-              </div>
-              <div
-                onClick={handleChange}
-                className="ml-4 w-[11.313rem] h-[11.313rem] cursor-pointer rounded-full shadow-[#189BA5]/20 shadow-xl top-[42.563rem] left-[63.461rem] bg-white"
-              >
-                <div className="flex flex-col items-center">
-                  <div className="pt-[1.688rem]">
-                    <Image src="/images/ls.svg" width={114} height={95} />
-                  </div>
-                  <div className="font-bold font-sans text-[1rem] text-[#189BA5]">
-                    Lok Sabha
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+            <Footer />
         </div>
-      </Fragment>
-      <Footer />
-    </div>
-  );
+    );
 }
