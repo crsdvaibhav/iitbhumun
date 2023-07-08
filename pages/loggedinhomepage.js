60b9ff6c7e473e61c571dfef39b1da85c9e7fab0
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2023-07-09 00:41:21 +0530

    Update loggedinhomepage.js

--- pages/loggedinhomepage.js
+++ pages/loggedinhomepage.js
@@ -38,28 +38,57 @@ const [ab,setstate]=useState(true)
   const [finaldata, setData] = useState([""]);
   
   
-useEffect(()=>
- {let Ref1 = ref(database,"records of Conference ambassadors/");
-onValue(Ref1, snapshot => {
- const data1=snapshot.val();
- const filteredData1 = filterDataByUserEmail(data1);
- if(filterDataByUserEmail(data1).length>0){
- setData(filteredData1)}
-})},[]);
-useEffect(()=>{
+// useEffect(()=>
+//  {let Ref1 = ref(database,"records of Conference ambassadors/");
+// onValue(Ref1, snapshot => {
+//  const data1=snapshot.val();
+//  const filteredData1 = filterDataByUserEmail(data1);
+//  if(filterDataByUserEmail(data1).length>0){
+//  setData(filteredData1)}
+// })
+
  
-   let Ref2 = ref(database,"records of single delegates/");
-onValue(Ref2, snapshot => {
- const data2=snapshot.val();
- const filteredData2 = filterDataByUserEmail(data2);
- if(filterDataByUserEmail(data2).length>0){
- setData(filteredData2)}
+//    let Ref2 = ref(database,"records of single delegates/");
+// onValue(Ref2, snapshot => {
+//  const data2=snapshot.val();
+//  const filteredData2 = filterDataByUserEmail(data2);
+//  if(filterDataByUserEmail(data2).length>0){
+//  setData(filteredData2)}
  
 
-})},[])
+// })},[])
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const Ref1 = ref(database, "records of Conference ambassadors/");
+      const snapshot1 = await get(Ref1);
+      const data1 = snapshot1.val();
+      const filteredData1 = filterDataByUserEmail(data1);
 
+      if (filteredData1.length > 0) {
+        setData(filteredData1);
+      }
 
+      const Ref2 = ref(database, "records of single delegates/");
+      const snapshot2 = await get(Ref2);
+      const data2 = snapshot2.val();
+      const filteredData2 = filterDataByUserEmail(data2);
+
+      if (filteredData2.length > 0) {
+        setData(filteredData2);
+      }
+    } catch (error) {
+      // Handle error
+      console.error(error);
+    }
+  };
+
+  fetchData();
+  
+}, []);
 console.log(finaldata)
+
+
 
 
 
@@ -94,7 +123,8 @@ return(
                 {/* Render other properties as needed */}
               </div>
             );
-          }
+            
+          }console.log("hello",finaldata)
           return null;
         })}
       </div>
