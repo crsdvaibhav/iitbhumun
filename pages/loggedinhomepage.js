18eaf176f352ac572331c8ad43f70ec9ff789568
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2023-07-09 09:44:27 +0530

    Update loggedinhomepage.js

--- pages/loggedinhomepage.js
+++ pages/loggedinhomepage.js
@@ -1,33 +1,44 @@
-// import React,{ useEffect, useState } from "react"
-// import { getAuth,signOut } from "firebase/auth"
-// import { Database, getDatabase,db,ref,onValue,get} from "firebase/database"
-// import app from "../public/firebaseconfig";
+
+// import React, { Component } from "react";
+// import { getAuth } from "firebase/auth";
+// import { getDatabase, ref, get } from "firebase/database";
 // import Footer from "../components/Footer";
-// import NavBar from '../components/Navbar';
-// import Countdown from '../components/Countdown';
+// import NavBar from "../components/Navbar";
+// import app from "../public/firebaseconfig";
 
 
 
 
 
-// const Loggedinhomepage = () => {
-    
-// const database=getDatabase();
-// const auth=getAuth();
-// let userEmail;
-// const [finaldata,setData]=useState([])
-// const [isLoading,setLoad]=useState(true);
-// const [ab,bc]=useState('initial');
-// // if (auth.currentUser) {
-// //   userEmail = auth.currentUser.email;
-// // } else {
-// //   userEmail = "shivanshu264@gmail.com";
-// // }
-//  userEmail = auth.currentUser ? auth.currentUser.email :"shivanshu264@gmail.com";
+// class Loggedinhomepage extends Component {
+  
+  
+  
 
-// // 
-// const filterDataByUserEmail = (data) => {
-//   if (data) {
+
+
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       finaldata: [],
+//       isLoading: true,
+//       userEmail: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     const database = getDatabase();
+//     const auth = getAuth();
+//     const userEmail = auth.currentUser ? auth.currentUser.email : "shivanshu264@gmail.com";
+
+//     this.setState({ userEmail }, () => {
+//       this.fetchData(database);
+//     });
+//   }
+
+//   filterDataByUserEmail(data) {
+//     const { userEmail } = this.state;
 //     const filteredData = Object.values(data).filter((item) => {
 //       const delegateData = Object.values(item);
 //       return delegateData.some((nestedItem) => {
@@ -36,142 +47,80 @@
 //     });
 //     return filteredData;
 //   }
-//   return [];
-// };
 
+//   async fetchData(database) {
+//     const Ref1 = ref(database, "records of Conference ambassadors/");
+//     const snapshot1 = await get(Ref1);
+//     const data1 = snapshot1.val();
+//     const filteredData1 = this.filterDataByUserEmail(data1);
 
-  
-  
-// useEffect(()=>
-//  {let Ref1 = ref(database,"records of Conference ambassadors/");
-// onValue(Ref1, snapshot => {
-//  const data1=snapshot.val();
-//  const filteredData1 = filterDataByUserEmail(data1);
-//  if(filterDataByUserEmail(data1).length>0){
-//  setData(filteredData1)}
-// })
- 
-//    let Ref2 = ref(database,"records of single delegates/");
-// onValue(Ref2, snapshot => {
-//  const data2=snapshot.val();
-//  const filteredData2 = filterDataByUserEmail(data2);
-//  if(filterDataByUserEmail(data2).length>0){
-//  setData(filteredData2)}
-//  setLoad(false)
+//     const Ref2 = ref(database, "records of single delegates/");
+//     const snapshot2 = await get(Ref2);
+//     const data2 = snapshot2.val();
+//     const filteredData2 = this.filterDataByUserEmail(data2);
 
-// })},
+//     const finaldata = [...filteredData1, ...filteredData2];
+//     const isLoading = false;
+//     this.setState({ finaldata, isLoading });
+//   }
 
-// []);
+//   render() {
+//     const { finaldata, isLoading } = this.state;
 
-
-
-
-
-  
-
-
-  
-
-// // useEffect(()=>{
-// //  fetchData()
- 
-// // console.log(finaldata)
-
-// // },[] )
-
-
-// if (isLoading===true||finaldata==[]){
-//   console.log("Loading");
-//   return (
-//     <>
-//     <NavBar navbar={true} backgroundColor="white" qt=''/>
-  
-//   <div className='h-25'>This is loading...</div>
-//   <Footer></Footer>
-// </>
-//   )
+//     if (isLoading || finaldata.length === 0) {
+//       return (
+//         <>
+//           <NavBar navbar={true} backgroundColor="white" qt="" />
+//           <div className=" ">This is loading...</div>
+//           <Footer />
+//         </>
+//       );
+//     } else {
+//       return (
+//         <>
+          
+//           <div>
+          
+//             <h2 className=" my-50 text-center font-bold">Record of this user:</h2>
+//             {finaldata.map((item) => {
+//               const delegateData = Object.values(item);
+//               const nestedItem = delegateData.find((item) => item.email === this.state.userEmail);
+//               if (nestedItem) {
+//                 return (
+//                   <div key={nestedItem.email} className="  font-bold text-center">
+//                     <p>Name: {nestedItem.name}</p>
+//                     <p>Age: {nestedItem.Age}</p>
+//                     <p>Gender: {nestedItem.Gender}</p>
+//                     <p>Email: {nestedItem.email}</p>
+//                   </div>
+//                 );
+//               }
+//               return null;
+//             })}
+//           </div>
+//           <Footer />
+//         </>
+//       );
+//     }
+//   }
 // }
-// else {
-//   console.log("success",finaldata);
-   
-//   return (
-//     <>
-//       <NavBar navbar={true} backgroundColor="white" qt="" />
-//       <div >
-//         <div >
-//         <h2 className="my-96">Record of this user:</h2>
-//         {finaldata.map((item) => {
-//           const delegateData = Object.values(item);
-//           const nestedItem = delegateData.find(
-//             (item) => item.email === userEmail
-//           );
-//           if (nestedItem) {
-//             return (
-//               <div key={nestedItem.email}>
-//                 <p>Name: {nestedItem.name}</p>
-//                 <p>Age: {nestedItem.Age}</p>
-//                 <p>Gender: {nestedItem.Gender}</p>
-//                 <p>Email: {nestedItem.email}</p>
-//                 {/* Render other properties as needed */}
-//               </div>
-//             );
-            
-//           }
-//           return (<div>Nothing Found</div>);
-//         })}
-//       </div>
-//         <Footer />
-//       </div>
-//     </>
-//   );
-      
-// };
 
-
-
-
-// }
 // export default Loggedinhomepage;
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import { getAuth } from "firebase/auth";
-import { getDatabase, ref, get } from "firebase/database";
+import { getDatabase, ref, get, onValue } from "firebase/database";
 import Footer from "../components/Footer";
 import NavBar from "../components/Navbar";
 import app from "../public/firebaseconfig";
+const Loggedinhomepage = () => {
+  const database = getDatabase();
+  const auth = getAuth();
+  const [finaldata, setFinalData] = useState([]);
+  const [isLoading, setLoad] = useState(true);
+  const userEmail = auth.currentUser ? auth.currentUser.email : "shivanshu264@gmail.com";
 
-
-
-
-
-class Loggedinhomepage extends Component {
-  
-  
-  
-
-
-
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      finaldata: [],
-      isLoading: true,
-      userEmail: "",
-    };
-  }
-
-  componentDidMount() {
-    const database = getDatabase();
-    const auth = getAuth();
-    const userEmail = auth.currentUser ? auth.currentUser.email : "shivanshu264@gmail.com";
-
-    this.setState({ userEmail }, () => {
-      this.fetchData(database);
-    });
-  }
-
-  filterDataByUserEmail(data) {
-    const { userEmail } = this.state;
+  // Function to filter data based on user email
+  const filterDataByUserEmail = (data) => {
     const filteredData = Object.values(data).filter((item) => {
       const delegateData = Object.values(item);
       return delegateData.some((nestedItem) => {
@@ -179,63 +128,69 @@ class Loggedinhomepage extends Component {
       });
     });
     return filteredData;
-  }
+  };
 
-  async fetchData(database) {
-    const Ref1 = ref(database, "records of Conference ambassadors/");
-    const snapshot1 = await get(Ref1);
-    const data1 = snapshot1.val();
-    const filteredData1 = this.filterDataByUserEmail(data1);
+  // Function to fetch data from the database
+  const fetchData = async () => {
+    try {
+      const Ref1 = ref(database, "records of Conference ambassadors/");
+      const snapshot1 = await get(Ref1);
+      const data1 = snapshot1.val();
+      const filteredData1 = filterDataByUserEmail(data1);
 
-    const Ref2 = ref(database, "records of single delegates/");
-    const snapshot2 = await get(Ref2);
-    const data2 = snapshot2.val();
-    const filteredData2 = this.filterDataByUserEmail(data2);
+      const Ref2 = ref(database, "records of single delegates/");
+      const snapshot2 = await get(Ref2);
+      const data2 = snapshot2.val();
+      const filteredData2 = filterDataByUserEmail(data2);
 
-    const finaldata = [...filteredData1, ...filteredData2];
-    const isLoading = false;
-    this.setState({ finaldata, isLoading });
-  }
-
-  render() {
-    const { finaldata, isLoading } = this.state;
-
-    if (isLoading || finaldata.length === 0) {
-      return (
-        <>
-          <NavBar navbar={true} backgroundColor="white" qt="" />
-          <div className=" ">This is loading...</div>
-          <Footer />
-        </>
-      );
-    } else {
-      return (
-        <>
-          
-          <div>
-          
-            <h2 className=" my-50 text-center font-bold">Record of this user:</h2>
-            {finaldata.map((item) => {
-              const delegateData = Object.values(item);
-              const nestedItem = delegateData.find((item) => item.email === this.state.userEmail);
-              if (nestedItem) {
-                return (
-                  <div key={nestedItem.email} className="  font-bold text-center">
-                    <p>Name: {nestedItem.name}</p>
-                    <p>Age: {nestedItem.Age}</p>
-                    <p>Gender: {nestedItem.Gender}</p>
-                    <p>Email: {nestedItem.email}</p>
-                  </div>
-                );
-              }
-              return null;
-            })}
-          </div>
-          <Footer />
-        </>
-      );
+      setFinalData([...filteredData1, ...filteredData2]);
+      
+    } catch (error) {
+      console.error(error);
     }
+  };
+
+  useEffect(() => {
+    fetchData()
+    .then(() =>
+    setLoad(false)
+    );
+   }, [])
+
+  if (isLoading || finaldata.length === 0) {
+    return (
+      <>
+        <NavBar navbar={true} backgroundColor="white" qt="" />
+        <div className="h-25">This is loading...</div>
+        <Footer />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <NavBar navbar={true} backgroundColor="white" qt="" />
+        <div className="my-50 text-center font-bold">
+          <h2 className="my-50 text-center font-bold">Record of this user:</h2>
+          {finaldata.map((item) => {
+            const delegateData = Object.values(item);
+            const nestedItem = delegateData.find((item) => item.email === userEmail);
+            if (nestedItem) { // Added null check for nestedItem
+              return (
+                <div key={nestedItem.email} className="my-40 display:'block' m-auto text-center font-bold">
+                  <p>Name: {nestedItem.name}</p>
+                  <p>Age: {nestedItem.Age}</p>
+                  <p>Gender: {nestedItem.Gender}</p>
+                  <p>Email: {nestedItem.email}</p>
+                </div>
+              );
+            }
+            return null;
+          })}
+        </div>
+        <Footer />
+      </>
+    );
   }
-}
+};
 
 export default Loggedinhomepage;
