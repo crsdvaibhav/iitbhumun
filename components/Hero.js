82bc0f5dfcacf1a6515ec7d3df4ec3d624e83061
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2023-08-06 16:13:02 +0530

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -40,19 +40,19 @@ export default function Hero() {
    
     <div className=''>
       <div className="sm:block hidden relative">
-        <div className="row-span-full col-span-full hero mx-0">
-          <div className='flex justify-center gap-60'>
+        <div className="row-span-full w-screen sm:overflow-x-scroll col-span-full hero mx-0">
+          <div className='flex justify-center gap-56'>
             <div className='leftside block'>
-          <h1 className='finaldate1 text-4xl text-[#B6FF1A] text-center md:text-8xl mt-96 ml-24 border-[#fff] border-solid'>IIT BHU MODEL</h1>
-          <h1 className='finaldate2 text-4xl text-[#B6FF1A] text-center md:text-8xl ml-24'>UNITED NATIONS</h1>
-          <h1 className='finaldate3 text-3xl text-[#fff] mt-4 text-center my-2 md:text-7xl ml-24'>RISE.SPEAK.RESOLVE</h1>
+          <h1 className='finaldate1 text-4xl text-[#B6FF1A] whitespace-nowrap text-center md:text-8xl mt-96 ml-24 border-[#fff] border-solid'>IIT BHU MODEL</h1>
+          <h1 className='finaldate2 text-4xl text-[#B6FF1A] text-center whitespace-nowrap md:text-8xl ml-24'>UNITED NATIONS</h1>
+          <h1 className='finaldate3 text-3xl text-[#fff] mt-4 text-center my-2 whitespace-nowrap md:text-7xl ml-24'>RISE.SPEAK.RESOLVE</h1>
           {changebar?<div className='flex gap-24 justify-center ml-4 mt-14'><Link href={'/registerpage'}><button className='bg-[#A3CF3ACC] hover:bg-green-500 hover:text-[#fff] text-4xl px-12 p-4 rounded-lg'>Register</button></Link>
          <Link href={'/loginpage'}><button className='bg-[#A3CF3ACC] hover:bg-green-500 hover:text-[#fff] text-4xl p-4 px-12 rounded-lg'>Login</button></Link></div>:<Link href={'/loggedinhomepage'}><Button className='block  mx-auto my-8 text-5xl'>View Profile</Button></Link>}
           </div>
           <div className='rightside block'>
-        <img className='block md:w-96 w-44 mx-auto mt-48' src='/images/Group.png'></img>
-          <h1 className=' finaldate text-4xl text-center text-[#fff] whitespace-nowrap md:text-8xl mt-18 ml-24 border-[#fff] border-solid'>29 SEPT - 1 OCT</h1>
-          <h1 className='  text-4xl text-[#fff] currentyear font-bold text-center mt-24 ml-24 border-[#fff] border-solid'>2023</h1>
+        <img className='block mb-12 md:w-72 w-44 mx-auto mt-48' src='/images/Group.png'></img>
+          <h1 className=' finaldate text-4xl text-center text-[#fff] whitespace-nowrap md:text-6xl mt-18 ml-24 border-[#fff] border-solid'>29 SEPT - 1 OCT</h1>
+          <h1 className='  text-4xl text-[#fff] currentyear font-bold text-center mt-16 ml-24 border-[#fff] border-solid'>2023</h1>
           </div>
         </div></div>
        
